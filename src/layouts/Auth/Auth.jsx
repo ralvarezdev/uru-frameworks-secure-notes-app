77fd4a69734999cbfbfb29481927654fb1bc6eeb
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-02-09 19:42:58 -0400

    refactor: improved auth login page

--- src/layouts/Auth/Auth.jsx
+++ src/layouts/Auth/Auth.jsx
@@ -1,5 +1,4 @@
 import './Auth.css'
-import Graphic from "../../components/Logo/Graphic/Graphic.jsx";
 import Title from "../../components/Text/Title/Title.jsx";
 import Separator from "../../components/Separator/Separator.jsx";
 import GraphicText from "../../components/Logo/GraphicText/GraphicText.jsx";
@@ -8,20 +7,17 @@ import GraphicText from "../../components/Logo/GraphicText/GraphicText.jsx";
 function Auth({title, children}) {
     return (
         <div className='main-container'>
-            <div className='left-container'>
-                <img src="/key--1980x1114.webp" alt="side-image"
-                     loading="lazy"
-                     className={'side-image'}/>
-            </div>
             <div className='right-container'>
-            <div className='content-container'>
-                <div className='title-container'>
-                    <Title className='title'>{title}</Title>
-                    <GraphicText className='logo'/>
+                <div className='content-container'>
+                    <div className="content">
+                        <div className='title-container'>
+                            <Title className='title'>{title}</Title>
+                            <GraphicText className='logo'/>
+                        </div>
+                        <Separator className='separator'/>
+                        {children}
+                    </div>
                 </div>
-                <Separator className='separator'/>
-                {children}
-            </div>
             </div>
         </div>
     )
